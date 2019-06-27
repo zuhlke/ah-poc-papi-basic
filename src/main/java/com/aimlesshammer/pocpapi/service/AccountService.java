92c27dea2aa07c1752179af2971abc28f4e10e43
Rik Howard <rik.howard@zuhlke.com>
2019-06-27 16:19:56 +0100

    Remove @Service annotation, which was preventing start-up

--- src/main/java/com/aimlesshammer/pocpapi/service/AccountService.java
+++ src/main/java/com/aimlesshammer/pocpapi/service/AccountService.java
@@ -13,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-@Service
 public class AccountService {
 
     private static final Logger logger = LoggerFactory.getLogger(AccountService.class);
