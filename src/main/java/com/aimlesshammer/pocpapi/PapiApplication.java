69d551bfdc6505778f626c200d213abfda15042b
Rik Howard <rik.howard@zuhlke.com>
2019-07-04 16:19:15 +0100

    Fix end-to-end test to work with the rest of the tests

--- src/main/java/com/aimlesshammer/pocpapi/PapiApplication.java
+++ src/main/java/com/aimlesshammer/pocpapi/PapiApplication.java
@@ -3,14 +3,20 @@ package com.aimlesshammer.pocpapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class PapiApplication {
 
     public static final String LOG_ID = "ah-poc-papi-logger";
+    private static ConfigurableApplicationContext context;
 
     public static void main(String[] args) {
-        SpringApplication.run(PapiApplication.class, args);
+        context = SpringApplication.run(PapiApplication.class, args);
+    }
+
+    public static void stop(Integer exitCode) {
+        SpringApplication.exit(context, () -> exitCode);
     }
 
 }
--- src/test/java/com/aimlesshammer/pocpapi/EndToEndTest.java
+++ src/test/java/com/aimlesshammer/pocpapi/EndToEndTest.java
@@ -3,24 +3,37 @@ package com.aimlesshammer.pocpapi;
 
 import java.util.List;
 import com.aimlesshammer.pocpapi.model.Account;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class EndToEndTest {
 
+    @Before
+    public void setUp() {
+        PapiApplication.main(new String[0]);
+    }
+
+    @After
+    public void tearDown() {
+        PapiApplication.stop(0);
+    }
+
     @Test
     public void itIs() {
-        PapiApplication.main(new String[0]);
         String expected = "[Account{type:credit;customerId:10101010;accountNumber:1234567890;balance:1234.50}, Account{type:current;customerId:10101010;accountNumber:64746383648;balance:34.50}]";
         String actual = new RestTemplateBuilder()
             .build()
             .exchange(
                 "http://localhost:8080/balance?customer-id=10101010",
                 HttpMethod.GET,
-                null,
+                new HttpEntity<>(new HttpHeaders()),
                 new ParameterizedTypeReference<List<Account>>() {}
             )
             .getBody()
