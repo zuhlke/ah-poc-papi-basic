a25c874b6aff77587a4d7847995df80455c1ade7
Rik Howard <rik.howard@zuhlke.com>
2019-07-04 15:39:34 +0100

    Remove redundant import

--- src/main/java/com/aimlesshammer/pocpapi/service/AccountService.java
+++ src/main/java/com/aimlesshammer/pocpapi/service/AccountService.java
@@ -11,7 +11,6 @@ import com.aimlesshammer.pocpapi.model.CreditAccount;
 import com.aimlesshammer.pocpapi.model.CurrentAccount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 public class AccountService {
 
